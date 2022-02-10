241dda5d70a0c63b6d512d2f103f2b84adb9853c
Tiago Ramos Alves <tiagoramosalves@hotmail.com>
2022-02-10 10:24:23 -0300

    create: Introducao a JavaScript

---
+++ introducao-js.js
@@ -0,0 +1,65 @@
+// var nomeVariavelVar = 'não se usa var';
+
+let valor1 = 11;
+let valor2 = '11';
+let resultado;
+let texto1 = "Variavel valor1: ";
+let resultVal1MaiorVal2;
+let resultVal1MenorVal2;
+let resultVal1MaiorOuIgualVal2;
+let resultVal1MenorOuIgualVal2;
+let resultVal1IgualVal2;
+//const nomeVariavelConst;
+
+/*
+Tipos de dados:
+Int = numeros inteiros
+String = 'texto'
+boolean = verdadeiro / falso
+*/
+
+/* Operadores matemáticos
++ - * / %
+*/
+//resultado = valor1 % valor2;
+
+/* Concatenar valores
+console.log("Variavel valor2: " + valor2);
+*/
+
+/* Operadores de comparação
+> maior que
+< menor que
+>= maior ou igual
+<= menor ou igual
+
+
+== valores iguais
+=== valores identicos (considerando o tipo dos valores)
+!== totalmente diferente (considerando o tipo dos valroes)
+
+
+
+*/
+
+/*resultVal1MaiorVal2 = valor1 > valor2;
+resultVal1MenorVal2 = valor1 < valor2;
+
+resultVal1IgualVal2 = valor1 !== valor2;
+
+console.log(texto1 + valor1);
+console.log("Variavel valor2: " + valor2);*/
+//console.log("Resultado: " + resultado);
+
+/*console.log("resultVal1MaiorVal2: " + resultVal1MaiorVal2);
+console.log("resultVal1MenorVal2: " + resultVal1MenorVal2 );
+/*console.log("resultVal1IgualVal2: " + resultVal1IgualVal2);*/
+
+
+ resultVal1MaiorOuIgualVal2 = valor1 == valor2;
+ resultVal1MenorOuIgualVal2 = valor1 === valor2;
+
+ console.log("res1: "+resultVal1MaiorOuIgualVal2);
+console.log("res2: "+resultVal1MenorOuIgualVal2);
+
+
